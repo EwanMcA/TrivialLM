44e770e64138be01c70191a3e54e9fd500b927ea
Ewan <ewan.m@hotmail.com>
2025-04-18 15:41:27 +0100

    use htmx properly

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,14 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 
 origins = [
     "http://localhost:5173",
@@ -19,9 +22,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/", response_class=HTMLResponse)
+def main(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="home.html"
+    )
 
 
 @app.post("/generate-trivia", response_class=HTMLResponse)
@@ -31,3 +36,14 @@ def generate_trivia():
         <h1>Look ma! HTML!</h1>
     </html>
     """
+
+@app.get("/leagues", response_class=HTMLResponse)
+def leagues(request: Request):
+    return templates.TemplateResponse(
+            request=request, name="leagues.html", context={
+                "leagues": [
+                    {"name": "League 1", "id": 1},
+                    {"name": "League 2", "id": 2}
+                ]
+            }
+    )
