6cb68583007f22b4f8593c3bd76215efc1b2b0f3
Ewan <ewan.m@hotmail.com>
2025-05-05 12:02:30 +0100

    refactor styles and fix form api

--- triviallm/main.py
+++ triviallm/main.py
@@ -1,3 +1,4 @@
+from google import genai
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
@@ -9,6 +10,8 @@ from fastapi.templating import Jinja2Templates
 from .db import SessionDep, create_db_and_tables
 from .models import League, Team
 
+
+gemini = genai.Client(api_key="AIzaSyDzsWDCBIFwazCvCes0kq5ZeEZ02Z7p1-w")
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -25,20 +28,62 @@ def main(request: Request):
     return templates.TemplateResponse(
         request=request, name="home.html", context={
             "leagues": [
-                {"name": "League 1", "id": 1},
-                {"name": "League 2", "id": 2}
+                {"name": "League 1", "id": 1, "teams": [{"name": "Team 1"}, {"name": "Team 2"}]},
+                {"name": "League 2", "id": 2, "teams": [{"name": "Team 1"}, {"name": "Team 2"}]},
             ]
         }
     )
 
+@app.get("/page-create-quiz", response_class=HTMLResponse)
+def create_quiz(request: Request):
+    return templates.TemplateResponse(
+        request=request, name="create_quiz.html", context={}
+    )
 
-@app.post("/generate-trivia", response_class=HTMLResponse)
-def generate_trivia():
-    return """
-    <html>
-        <h1>Look ma! HTML!</h1>
-    </html>
-    """
+
+class QuizCreate(BaseModel):
+    num_questions: int
+    difficulty: str
+    topics: list[str]
+
+
+class Question(BaseModel):
+    question: str
+    choices: list[str]
+    answer: str
+
+
+class Quiz(BaseModel):
+    questions: list[Question]
+
+
+@app.post("/generate-quiz", response_class=HTMLResponse)
+def generate_quiz(request: Request, body: QuizCreate):
+    response = gemini.models.generate_content(
+        model="gemini-2.0-flash",
+        contents=(
+            f"Generate a quiz with {body['num_questions']} questions. "
+            f"Difficulty: {body['difficulty']}. "
+            "Include questions from the following categories (if True): "
+            f"General Knowledge: {body['general_knowledge']}. "
+            f"History: {body['history']}. "
+            f"Science: {body['science']}. "
+            f"Geography: {body['geography']}. "
+            f"Entertainment (Books): {body['entertainment_books']}. "
+            f"Entertainment (Film/TV): {body['entertainment_film_tv']}."
+        ),
+        config={
+        'response_mime_type': 'application/json',
+        'response_schema': Quiz,
+    },
+    )
+
+    return templates.TemplateResponse(
+        request=request, name="quiz.html", context={
+            "quiz": response.parsed,
+        }
+    )
+
 
 @app.get("/leagues/{league_id}", response_class=HTMLResponse)
 def league(request: Request, league_id: int, session: SessionDep):
